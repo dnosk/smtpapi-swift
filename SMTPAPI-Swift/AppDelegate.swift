9eb45764cc980bfe647d1d40e395119624857e7c
Scott K <scott.kawai@sendgrid.com>
2014-10-23 16:13:43 -0700

    Initial Commit

---
+++ SMTPAPI-Swift/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SMTPAPI-Swift
+//
+//  Created by Scott Kawai on 10/23/14.
+//  Copyright (c) 2014 SendGrid. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
