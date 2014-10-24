e910a243d2cb36c33c5c12df35f67513af617ff1
Scott K <scott.kawai@sendgrid.com>
2014-10-23 17:41:20 -0700

    Added tests for the add to methods

--- SMTPAPI-SwiftTests/SMTPAPI_SwiftTests.swift
+++ SMTPAPI-SwiftTests/SMTPAPI_SwiftTests.swift
@@ -22,12 +22,33 @@ class SMTPAPI_SwiftTests: XCTestCase {
     }
     
     func testAddTo() {
-        var header1 = SmtpApi()
-        header1.addTo("foo@bar.com", name: nil)
-        XCTAssertEqual(header1.jsonValue, "{\"to\":[\"foo@bar.com\"]}", "Adds a single address with no name.")
-        var header2 = SmtpApi()
-        header2.addTo("foo@bar.com", name: "Foo Bar")
-        XCTAssertEqual(header2.jsonValue, "{\"to\":[\"Foo Bar <foo@bar.com>\"]}", "Adds a single address with a to name.")
+        var header = SmtpApi()
+        header.addTo("foo@bar.com", name: nil)
+        XCTAssertEqual(header.jsonValue, "{\"to\":[\"foo@bar.com\"]}", "Adds a single address with no name.")
+        
+        header.addTo("foo@bar.com", name: "Foo Bar")
+        XCTAssertEqual(header.jsonValue, "{\"to\":[\"foo@bar.com\",\"Foo Bar <foo@bar.com>\"]}", "Adds a single address with a to name.")
+    }
+    
+    func testAddTos() {
+        var header = SmtpApi()
+        header.addTos(["foo@bar.com","bar@foo.com"], names: nil)
+        XCTAssertEqual(header.jsonValue, "{\"to\":[\"foo@bar.com\",\"bar@foo.com\"]}", "Adds multiple addresses with no names.")
+        
+        header.addTos(["foo@bar.com","bar@foo.com"], names: ["Foo", "Bar"])
+        XCTAssertEqual(header.jsonValue, "{\"to\":[\"foo@bar.com\",\"bar@foo.com\",\"Foo <foo@bar.com>\",\"Bar <bar@foo.com>\"]}", "Adds multiple addresses with  to names.")
+    }
+    
+    func testSetTos() {
+        var header = SmtpApi()
+        header.setTos(["foo@bar.com"], names: nil)
+        XCTAssertEqual(header.jsonValue, "{\"to\":[\"foo@bar.com\"]}", "Sets multiple addresses with no existing addresses and no to names.")
+        
+        header.setTos(["foo@bar.com","bar@foo.com"], names: nil)
+        XCTAssertEqual(header.jsonValue, "{\"to\":[\"foo@bar.com\",\"bar@foo.com\"]}", "Sets multiple addresses with pre-existing addresses and no to names.")
+        
+        header.setTos(["foo@bar.com","bar@foo.com"], names: ["Foo", "Bar"])
+        XCTAssertEqual(header.jsonValue, "{\"to\":[\"Foo <foo@bar.com>\",\"Bar <bar@foo.com>\"]}", "Sets multiple addresses with pre-existing addresses and to names.")
     }
     
 }
